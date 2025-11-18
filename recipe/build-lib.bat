3ca94861d0fdc32de8492de867cf6e6a78c7a5dc
H. Vetinari <h.vetinari@gmx.com>
2025-11-18 18:12:45 +1100

    ensure both shared & static outputs have correct CMake metadata

--- recipe/build-lib.bat
+++ recipe/build-lib.bat
@@ -1,11 +1,22 @@
+@echo on
+setlocal enabledelayedexpansion
+
 CFLAGS="${CFLAGS} -O3"
 CXXFLAGS="${CXXFLAGS} -O3"
+
+if [%PKG_NAME%] == [pcre2] (
+    REM first without static libs...
+    set "CF_BUILD_STATIC_LIBS=OFF"
+) else (
+    REM ... then with
+    set "CF_BUILD_STATIC_LIBS=ON"
+)
 
 mkdir "%SRC_DIR%"\build_cmake
 pushd "%SRC_DIR%"\build_cmake
 cmake %CMAKE_ARGS% ^
     -DBUILD_SHARED_LIBS=ON ^
-    -DBUILD_STATIC_LIBS=ON ^
+    -DBUILD_STATIC_LIBS=!CF_BUILD_STATIC_LIBS! ^
     -DCMAKE_BUILD_TYPE=release ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPCRE2_SUPPORT_JIT=ON ^
@@ -15,13 +26,19 @@ cmake %CMAKE_ARGS% ^
     ..
 if errorlevel 1 exit 1
 
-ninja
+cmake --build .
 if errorlevel 1 exit 1
 sed -ie "s/\$<TARGET_FILE:pcre2test>/pcre2test.exe/" pcre2_test.bat
 cp -r ..\testdata .
 if errorlevel 1 exit 1
 
 if not "%CONDA_BUILD_CROSS_COMPILATION%" == "1" (
-  ninja test
+  ctest --rerun-failed --output-on-failure
   if errorlevel 1 exit 1
 )
+
+cmake --install .
+if errorlevel 1 exit 1
+
+popd
+rmdir /s /q "%SRC_DIR%"\build_cmake
